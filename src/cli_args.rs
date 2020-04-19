0b0d13da3453fac63cd96130bc03d4be7b9135fc
Taylan Dogan <doganntaylan@gmail.com>
2020-04-19 17:23:42 +0300

    feat: add cli args
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

---
+++ src/cli_args.rs
@@ -0,0 +1,28 @@
+use structopt::StructOpt;
+
+/// List of the command line arguments
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(name = "basic")]
+pub(crate) struct Opt {
+    /// Listing existing OTP secrets.
+    #[structopt(short, long)]
+    pub list: Option<bool>,
+
+    /// Adding new OTP secret
+    #[structopt(short, long)]
+    pub add: Option<String>,
+
+    /// Remove existing OTP secret
+    #[structopt(short, long)]
+    pub delete: Option<String>,
+}
+
+impl Opt {
+    /// Parses CLI arguments
+    pub(crate) fn get_cli_args() -> Self
+    where
+        Self: Sized,
+    {
+        Opt::from_args()
+    }
+}
