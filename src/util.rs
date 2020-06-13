b6fa738f44274d5d481a683d714223618d4eca80
Taylan Dogan <doganntaylan@gmail.com>
2020-06-13 21:25:14 +0300

    refactor(utils): stop using early returns
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- src/util.rs
+++ src/util.rs
@@ -6,7 +6,7 @@ use dirs::config_dir;
 use rpassword::read_password_from_tty;
 use std::fs::{
     create_dir_all,
-    remove_dir,
+    remove_dir_all,
 };
 use std::io;
 use std::path::{
@@ -16,11 +16,19 @@ use std::path::{
 
 /// Checks if the given path exists or not
 pub(crate) fn does_path_exists(path: &str) -> bool {
-    if let Some(p) = get_config_dir() {
-        let notp_path = format!("{}/{}", p.to_str().unwrap_or_default(), path);
-        return Path::new(&notp_path).exists();
-    }
-    false
+    get_config_dir()
+        .map_or_else(
+            || Err(NotpError::Generic("Path does not exists.".to_string())),
+            |path_buf| {
+                let notp_path = format!(
+                    "{}/{}",
+                    path_buf.to_str().unwrap_or_default(),
+                    path
+                );
+                Ok(Path::new(&notp_path).exists())
+            },
+        )
+        .unwrap_or_else(|_| false)
 }
 
 fn get_config_dir() -> Option<PathBuf> {
@@ -29,28 +37,44 @@ fn get_config_dir() -> Option<PathBuf> {
 
 /// Creates folder inside the config folder.
 pub(crate) fn create_folder(path: &str) -> NotpResult<()> {
-    if let Some(p) = get_config_dir() {
-        let notp_path = format!("{}/{}", p.to_str().unwrap_or_default(), path);
-        return match create_dir_all(&notp_path) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(NotpError::Io(e)),
-        };
+    if path.is_empty() {
+        Err(NotpError::Generic(
+            "Path is empty! Provide a path!".to_string(),
+        ))
+    } else {
+        get_config_dir().map_or_else(
+            || Err(NotpError::Generic("Cannot get PathBuf.".to_string())),
+            |path_buf| {
+                let notp_path = format!(
+                    "{}/{}",
+                    path_buf.to_str().unwrap_or_default(),
+                    path
+                );
+                create_dir_all(&notp_path).map_err(|e| NotpError::Io(e))
+            },
+        )
     }
-    Err(NotpError::Generic("Cannot find the file!".to_string()))
 }
 
+/// Takes a folder path and removes the folder's content and folder itself. And
+/// returns to `NotpResult`.
 #[allow(dead_code)]
 pub(crate) fn remove_folder(path: &str) -> NotpResult<()> {
     if does_path_exists(&path) {
-        if let Some(p) = get_config_dir() {
-            let folder_path =
-                format!("{}/{}", p.to_str().unwrap_or_default(), path);
-            return remove_dir(folder_path)
-                .map(|_| ())
-                .map_err(|e| NotpError::Generic(e.to_string()));
-        }
+        get_config_dir().map_or_else(
+            || Err(NotpError::Generic("Cannot get PathBuf.".to_string())),
+            |path_buf| {
+                let folder_path = format!(
+                    "{}/{}",
+                    path_buf.to_str().unwrap_or_default(),
+                    path
+                );
+                remove_dir_all(folder_path).map_err(|e| NotpError::Io(e))
+            },
+        )
+    } else {
+        Err(NotpError::Generic("Cannot Find the folder!".to_string()))
     }
-    Err(NotpError::Generic("Config path does not exist".to_string()))
 }
 
 /// Returns to the full path of the given path.
@@ -59,12 +83,10 @@ pub(crate) fn remove_folder(path: &str) -> NotpResult<()> {
 /// saying `get_folder_path("notp")` and this function will return to the
 /// full path such as `/home/user/.config/notp`.
 pub(crate) fn get_folder_path(path: &str) -> Option<String> {
-    if let Some(p) = get_config_dir() {
-        let folder_path =
-            format!("{}/{}", p.to_str().unwrap_or_default(), path);
-        return Some(folder_path);
-    }
-    None
+    get_config_dir().map_or_else(
+        || None,
+        |p| Some(format!("{}/{}", p.to_str().unwrap_or_default(), path)),
+    )
 }
 
 /// Reads text from stdin
